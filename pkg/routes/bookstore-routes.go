e16da922e961d89293334e2422a6f89c6f8d5c64
saaib8 <saaibahmed456@gmail.com>
2025-06-02 12:19:26 +0500

    Project Structure

---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/saaib8/go-bookstore/pkg/controllers"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router){
+	router.HandleFunc("/book/",controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/",controllers.GetBook).Methods("Get")
+	router.HandleFunc("/book/{bookId}",controllers.CreateBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}",controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}",controllers.DeleteBook).Methods("DELETE")
+}
